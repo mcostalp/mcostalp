74cfd3979a38ca9da63419c2c28675456d991627
Luis Paulo <mcostalp@gmail.com>
2022-05-12 19:10:11 -0300

    Exercício 4.1 pt1

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -0,0 +1,14 @@
+let a = 10;
+let b = 20;
+
+adicao = (a+b);
+subtracao = (a-b);
+multiplicacao = (a*b);
+divisao = (a/b);
+modulo = (a%b);
+
+console.log(adicao);
+console.log(subtracao);
+console.log(multiplicacao);
+console.log(divisao);
+console.log(modulo);

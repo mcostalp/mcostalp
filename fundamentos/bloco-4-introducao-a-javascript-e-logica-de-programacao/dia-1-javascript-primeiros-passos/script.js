dc4fe50e7d5173d872a10e7e28f3d47ad049fb4e
Luis Paulo <mcostalp@gmail.com>
2022-05-16 19:20:22 -0300

    4.1

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -1,14 +1,66 @@
-let a = 10;
-let b = 20;
+//Parte 1-----
+// let a = 10;
+// let b = 20;
 
-adicao = (a+b);
-subtracao = (a-b);
-multiplicacao = (a*b);
-divisao = (a/b);
-modulo = (a%b);
+// adicao = (a+b);
+// subtracao = (a-b);
+// multiplicacao = (a*b);
+// divisao = (a/b);
+// modulo = (a%b);
 
-console.log(adicao);
-console.log(subtracao);
-console.log(multiplicacao);
-console.log(divisao);
-console.log(modulo);
+// console.log(adicao);
+// console.log(subtracao);
+// console.log(multiplicacao);
+// console.log(divisao);
+// console.log(modulo);
+
+//Parte 2-----
+// let a = 5
+// let b = 3
+
+// if (a<b){
+//     console.log(b);
+// }else (a>b){
+//     console.log(a);
+// };
+
+//-----Parte 3------
+const num1 = 5
+const num2 = 10
+const num3 = 11
+let maior = num1;
+
+if (maior < num2) {
+    maior = num2
+}
+
+if(maior < num3) {
+    maior = num3
+}
+
+console.log('O maior é', maior);
+
+//-----Parte 4-----
+// const num = 5
+
+// if (num>0) {
+//     console.log('Positivo');
+// }else if (num<0) {
+//     console.log(Negativo);
+// }else{
+//     console.log('Zero');
+// };
+
+
+//-----Parte 5-----
+// const pAngulo = 45
+// const sAngulo = 45
+// const tAngulo = 90
+
+// soma = (pAngulo + sAngulo + tAngulo)
+// if (soma === 180) {
+//     soma = true;
+// }else{
+//     soma = false;
+// };
+// console.log(soma);

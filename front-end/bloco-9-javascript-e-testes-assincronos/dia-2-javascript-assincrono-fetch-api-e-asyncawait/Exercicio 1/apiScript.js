64260c88c6b2d89783a90f8e13486c5ffe1debf2
luis <mcostalp@gmail.com>
2022-07-07 19:12:40 -0300

    Separando exercícios

---
+++ front-end/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/Exercicio 1/apiScript.js
@@ -0,0 +1,16 @@
+// apiScript.js
+const API_URL = 'https://icanhazdadjoke.com/';
+const h2 = document.getElementById('jokeContainer');
+
+const fetchJoke = () => {
+  const myObject = {
+    method: 'GET',
+    headers: { 'Accept': 'application/json' }
+  };
+
+  fetch(API_URL, myObject)
+    .then(response => response.json())
+    .then(data => h2.innerHTML = data.joke)
+};
+
+window.onload = () => fetchJoke();
